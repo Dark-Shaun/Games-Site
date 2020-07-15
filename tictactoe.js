90626abaa647bc17a562802d91d7503f507ab556
Dark-Shaun <63874836+Dark-Shaun@users.noreply.github.com>
2020-07-15 14:54:56 +0530

    Delete tictactoe.js

--- tictactoe.js
+++
@@ -1,7 +0,0 @@
-var my=document.getElementById("secondhead")
-function openWin() {
-  window.open("game.html");
-}
-my.addEventListener("mouseover",function(){my.style.fontWeight='bold';})
-my.addEventListener("mouseout",function(){my.style.fontWeight='normal';})
-my.addEventListener("click",openWin)
